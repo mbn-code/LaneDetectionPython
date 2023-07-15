51d152c2a242c57ffc40ac2c1d971ff81053a259
CollinEdward <manorengaard@icloud.com>
2023-07-16 00:25:47 +0200

    Lane length improvement

--- lane_detectionMain.py
+++ lane_detectionMain.py
@@ -1,99 +1,93 @@
 import cv2
 import numpy as np
 
-def region_of_interest(img, vertices):
-    mask = np.zeros_like(img)
-    cv2.fillPoly(mask, vertices, 255)
-    masked_img = cv2.bitwise_and(img, mask)
-    return masked_img
+def lane_detection(image):
+    # Convert the image to grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-def draw_lines(img, lines, color=(0, 0, 255), thickness=3):
+    # Apply Gaussian blur to reduce noise
+    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+
+    # Apply Canny edge detection
+    edges = cv2.Canny(blurred, 50, 150)
+
+    # Define a region of interest (ROI)
+    height, width = edges.shape
+    roi_vertices = [
+        (0, height),
+        (width / 2, height / 2),
+        (width, height)
+    ]
+    mask = np.zeros_like(edges)
+    cv2.fillPoly(mask, np.array([roi_vertices], dtype=np.int32), 255)
+    masked_edges = cv2.bitwise_and(edges, mask)
+
+    # Apply Hough line transform to detect lines
+    lines = cv2.HoughLinesP(masked_edges, rho=1, theta=np.pi/180, threshold=50, minLineLength=100, maxLineGap=50)
+
+    # Check if lines are detected
     if lines is not None:
+        # Calculate the slope and intercept of each line
+        left_lines = []
+        right_lines = []
         for line in lines:
-            for x1, y1, x2, y2 in line:
-                cv2.line(img, (x1, y1), (x2, y2), color, thickness)
-
-def calculate_center_line(lines, height, width):
-    left_lines = []
-    right_lines = []
-
-    if lines is not None:
-        for line in lines:
-            for x1, y1, x2, y2 in line:
-                slope = (y2 - y1) / (x2 - x1)
-                intercept = y1 - slope * x1
-                if slope < -0.3 and intercept > width / 2:
-                    left_lines.append(line)
-                elif slope > 0.3 and intercept < width / 2:
-                    right_lines.append(line)
-
-    left_slope, left_intercept = calculate_average_slope_intercept(left_lines)
-    right_slope, right_intercept = calculate_average_slope_intercept(right_lines)
-
-    y1 = height
-    y2 = int(height * 0.6)
-
-    left_x1 = int((y1 - left_intercept) / left_slope) if left_slope != 0 else int(width / 2)
-    left_x2 = int((y2 - left_intercept) / left_slope) if left_slope != 0 else int(width / 2)
-    right_x1 = int((y1 - right_intercept) / right_slope) if right_slope != 0 else int(width / 2)
-    right_x2 = int((y2 - right_intercept) / right_slope) if right_slope != 0 else int(width / 2)
-
-    return [(left_x1, y1, left_x2, y2), (right_x1, y1, right_x2, y2)]
-
-def calculate_average_slope_intercept(lines):
-    slopes = []
-    intercepts = []
-
-    for line in lines:
-        for x1, y1, x2, y2 in line:
+            x1, y1, x2, y2 = line[0]
             slope = (y2 - y1) / (x2 - x1)
             intercept = y1 - slope * x1
-            slopes.append(slope)
-            intercepts.append(intercept)
+            if slope < 0:
+                left_lines.append((slope, intercept))
+            else:
+                right_lines.append((slope, intercept))
 
-    average_slope = np.mean(slopes) if slopes else 0
-    average_intercept = np.mean(intercepts) if intercepts else 0
+        # Calculate the average slope and intercept for left and right lanes
+        if len(left_lines) > 0:
+            left_slope, left_intercept = np.mean(left_lines, axis=0)
+        else:
+            left_slope, left_intercept = np.nan, np.nan
 
-    return average_slope, average_intercept
+        if len(right_lines) > 0:
+            right_slope, right_intercept = np.mean(right_lines, axis=0)
+        else:
+            right_slope, right_intercept = np.nan, np.nan
 
-def lane_detection(image):
-    if len(image.shape) > 2 and image.shape[2] > 1:
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        # Calculate the coordinates of the lane lines
+        y1 = height
+        y2 = int(height / 2)
+        left_x1 = int((y1 - left_intercept) / left_slope)
+        left_x2 = int((y2 - left_intercept) / left_slope)
+        right_x1 = int((y1 - right_intercept) / right_slope)
+        right_x2 = int((y2 - right_intercept) / right_slope)
+
+        # Draw the lane lines on the image
+        lane_image = np.zeros_like(image)
+        cv2.line(lane_image, (left_x1, y1), (left_x2, y2), (0, 255, 0), 5)
+        cv2.line(lane_image, (right_x1, y1), (right_x2, y2), (0, 255, 0), 5)
+
+        # Overlay the lane lines on the original image
+        result = cv2.addWeighted(image, 0.8, lane_image, 1, 0)
+
+        return result
     else:
-        gray = image
-
-    edges = cv2.Canny(gray, 100, 200)
-
-    height, width = edges.shape[:2]
-    vertices = np.array([[(0, height), (width / 2, height / 2), (width, height)]], dtype=np.int32)
-    masked_edges = region_of_interest(edges, vertices)
-
-    lines = cv2.HoughLinesP(masked_edges, rho=1, theta=np.pi / 180, threshold=50, minLineLength=150, maxLineGap=50)
-
-    line_image = np.zeros_like(image)
-    draw_lines(line_image, lines)
-
-    center_line = calculate_center_line(lines, height, width)
-    cv2.line(line_image, center_line[0][:2], center_line[0][2:], (0, 255, 0), 5)
-    cv2.line(line_image, center_line[1][:2], center_line[1][2:], (0, 255, 0), 5)
-
-    lane_image = cv2.addWeighted(image, 0.8, line_image, 1, 0)
-    return lane_image
+        return image
 
 # Read the input video
-cap = cv2.VideoCapture('/Users/mbn/Documents/Programmering/python3/LaneDetectionPython/Better test.mp4')
+video = cv2.VideoCapture("Better test.mp4")
 
 while True:
-    ret, frame = cap.read()
-
+    ret, frame = video.read()
     if not ret:
         break
 
-    lane_frame = lane_detection(frame)
-    cv2.imshow('Lane Detection', lane_frame)
+    # Perform lane detection on each frame
+    result = lane_detection(frame)
 
+    # Display the result
+    cv2.imshow("Lane Detection", result)
+
+    # Exit if 'q' is pressed
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-cap.release()
+# Release the video capture and close all windows
+video.release()
 cv2.destroyAllWindows()
