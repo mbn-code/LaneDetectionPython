197f880412cafd8008490052df7a1a5cacb0ffb2
1cy50ng3b0b <66748817+CollinEdward@users.noreply.github.com>
2023-07-20 16:03:20 +0200

    Add files via upload

---
+++ car_detection.py
@@ -0,0 +1,86 @@
+import cv2
+import numpy as np
+
+# Load YOLOv3 Tiny model
+net = cv2.dnn.readNet("yolov3-tiny.weights", "yolov3-tiny.cfg")
+
+# Load class labels
+classes = []
+with open("coco.names", "r") as f:
+    classes = [line.strip() for line in f.readlines()]
+
+# Get output layer names
+layer_names = net.getLayerNames()
+output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+# Load video
+video_path = "/Users/mbn/Documents/Programmering/python3/LaneDetectionPython/Better test.mp4"
+cap = cv2.VideoCapture(video_path)
+
+while True:
+    ret, frame = cap.read()
+    
+    if not ret:
+        break
+
+    # Preprocess frame
+    blob = cv2.dnn.blobFromImage(frame, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+    net.setInput(blob)
+
+    # Perform object detection
+    outs = net.forward(output_layers)
+
+    # Process detection results
+    class_ids = []
+    confidences = []
+    boxes = []
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+
+            if confidence > 0.5 and class_id == 2:  # Class ID 2 represents cars
+                # Scale the bounding box coordinates back to the original frame size
+                width = frame.shape[1]
+                height = frame.shape[0]
+                center_x = int(detection[0] * width)
+                center_y = int(detection[1] * height)
+                w = int(detection[2] * width)
+                h = int(detection[3] * height)
+
+                # Calculate the top-left corner coordinates of the bounding box
+                x = int(center_x - w / 2)
+                y = int(center_y - h / 2)
+
+                # Store the detection results
+                class_ids.append(class_id)
+                confidences.append(float(confidence))
+                boxes.append([x, y, w, h])
+
+    # Apply non-max suppression to remove redundant detections
+    indices = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+
+    # Draw bounding boxes and labels on the frame
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    colors = np.random.uniform(0, 255, size=(len(classes), 3))
+
+    if len(indices) > 0:
+        for i in indices.flatten():
+            x, y, w, h = boxes[i]
+            label = classes[class_ids[i]]
+            confidence = confidences[i]
+            color = colors[class_ids[i]]
+
+            cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+            cv2.putText(frame, f"{label}: {confidence:.2f}", (x, y - 5), font, 0.5, color, 2)
+
+    # Display the output frame
+    cv2.imshow("Car Detection", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the video capture and close the window
+cap.release()
+cv2.destroyAllWindows()
